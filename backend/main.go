f15cf9be843c670ecdec91024e769c59e2b82c8b
Joshua Dall'Acqua <joshd@localhost.localdomain>
2022-08-27 09:49:59 -0400

    serev to lan

--- backend/main.go
+++ backend/main.go
@@ -9,8 +9,8 @@ func main() {
 	fs := http.FileServer(http.Dir("../"))
 	http.Handle("/", fs)
 
-	log.Print("Listening on https://localhost:3000")
-	err := http.ListenAndServe(":3000", nil)
+	log.Print("Listening on 192.168.1.173")
+	err := http.ListenAndServe("192.168.1.173:3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
